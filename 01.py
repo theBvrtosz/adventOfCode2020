b7749209d609864638a40921e182efe73a74687d
Bart <bartosz.gejgal@gmail.com>
2020-12-02 12:50:25 +0100

    fixed [1010, 1, 2019] bug

--- 01.py
+++ 01.py
@@ -1,13 +1,14 @@
 def part1_solution(inp):
-    for n in inp:
-        lookup_n = 2020 - n
-        if lookup_n in inp:
-            return (n,lookup_n, n*lookup_n)
+    for n in range(len(inp)):
+        first_n = inp[n]
+        lookup_n = 2020 - first_n
+        if lookup_n in inp[n+1:]:
+            return (first_n,lookup_n, n*lookup_n)
 
 def part2_solution(inp):
     for n in range(len(inp)):
         first_n = inp[n]
-        for second_n in inp[:n] + inp[n+1:]:
+        for second_n in inp[n+1:]:
             lookup_n = 2020-first_n-second_n
             if lookup_n in inp:
                 return(first_n, second_n, lookup_n, first_n*second_n*lookup_n)
